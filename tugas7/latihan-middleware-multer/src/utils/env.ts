f883e3ae0718075e86a1d17e2cebdfb56f43d08b
Dzakky Budiman <dzakkybudiman19@gmail.com>
2024-06-26 01:25:25 +0700

    fix

---
+++ tugas7/latihan-middleware-multer/src/utils/env.ts
@@ -0,0 +1,5 @@
+export const CLOUDINARY_API_KEY: string = process.env.CLOUDINARY_API_KEY || "638899148261883";
+export const CLOUDINARY_API_SECRET: string =
+  process.env.CLOUDINARY_API_SECRET || "1P4RJ6HxLe3vbK-DtSp3mm3twFU";
+export const CLOUDINARY_CLOUD_NAME: string =
+  process.env.CLOUDINARY_CLOUD_NAME || "djzt7hb9c";
