550595c218a2e0d6e3d8eb6daf9123af60ca023b
Dzakky Budiman <dzakkybudiman19@gmail.com>
2024-06-26 01:24:14 +0700

    tugas7

---
+++ tugas7/latihan-middleware-multer/src/server.ts
@@ -0,0 +1,12 @@
+import express from "express";
+import router from "./route";
+
+const app = express();
+const PORT = 3000;
+
+app.use(express.json());
+app.use(router);
+
+app.listen(PORT, () => {
+  console.log(`Server is running at http://localhost:${PORT}`);
+});
