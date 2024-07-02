7ada7f4d435eec0cc195e180f22f572dc58b6ec2
Dzakky Budiman <dzakkybudiman19@gmail.com>
2024-07-02 15:47:49 +0700

    deploy

---
+++ deploy/src/utils/env.ts
@@ -0,0 +1,11 @@
+export const CLOUDINARY_API_KEY: string = process.env.CLOUDINARY_API_KEY || "638899148261883";
+export const CLOUDINARY_API_SECRET: string =
+  process.env.CLOUDINARY_API_SECRET || "1P4RJ6HxLe3vbK";
+export const CLOUDINARY_CLOUD_NAME: string =
+  process.env.CLOUDINARY_CLOUD_NAME || "djzt7hb9c";
+export const DATABASE_URL: string = process.env.DATABASE_URL || "mongodb+srv://budimansol:november@belajar.wsdokw6.mongodb.net/?retryWrites=true&w=majority&appName=Belajar";
+
+import dotenv from 'dotenv';
+dotenv.config();
+
+export const SECRET: string = process.env.SECRET || "12345678901234567890123456789012";
